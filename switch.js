dd9dc7d75bb7103cbf52e94f057edbabaf0be6ce
raiden-id <krulchifer01@gmail.com>
2023-09-30 08:24:37 +0800

    switch

---
+++ switch.js
@@ -0,0 +1,13 @@
+let str = "Hello";
+
+switch (str) {
+  case "Hello":
+    console.log("Hello");
+    break;
+  case "World":
+    console.log("World");
+    break;
+  default:
+    console.log(str, "Default");
+    break;
+}
